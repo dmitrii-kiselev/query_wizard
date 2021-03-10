37d216a8b9aa2d6e04ce56f8e53775a14d5199db
Dmitrii Kiselev <dmitrii.kiselev.dev@gmail.com>
2021-03-11 01:36:45 +0300

    feat(lib): add internationalization

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/query_wizard_localizations.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,6 +23,10 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
+      localizationsDelegates: const [
+        ...QueryWizardLocalizations.localizationsDelegates,
+      ],
+      supportedLocales: QueryWizardLocalizations.supportedLocales,
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
