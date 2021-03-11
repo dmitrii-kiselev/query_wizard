9777dfe5fb3146089e3b02d018cfaccf9eadb4ee
Dmitrii Kiselev <dmitrii.kiselev.dev@gmail.com>
2021-03-12 00:47:14 +0300

    feat(lib): add tabs

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,36 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:query_wizard/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Query Wizard initialized smoke test',
+      (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(QueryWizard());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that widgets exists.
+    expect(find.text('Query Wizard'), findsOneWidget);
+    expect(find.text('Tables and fields'), findsNWidgets(2));
+    expect(find.text('Joins'), findsOneWidget);
+    expect(find.text('Grouping'), findsOneWidget);
+    expect(find.text('Conditions'), findsOneWidget);
+    expect(find.text('More'), findsOneWidget);
+    expect(find.text('Unions/Aliases'), findsOneWidget);
+    expect(find.text('Order'), findsOneWidget);
+    expect(find.text('Query batch'), findsOneWidget);
+  });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  testWidgets('Tabs changes smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(QueryWizard());
+    await tester.tap(find.text('Joins'));
+    await tester.pumpAndSettle();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that widgets exists.
+    expect(find.text('Tables and fields'), findsOneWidget);
+    expect(find.text('Joins'), findsNWidgets(2));
   });
 }
