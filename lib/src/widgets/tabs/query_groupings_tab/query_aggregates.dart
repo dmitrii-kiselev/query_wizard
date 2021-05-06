71bc811bdfa167c381de0b207c3729b49d1f541d
Dmitrii Kiselev <dmitrii.kiselev.dev@gmail.com>
2021-05-06 21:01:45 +0300

    feat(lib): add query groupings selection lists

--- lib/src/widgets/tabs/query_groupings_tab/query_aggregates.dart
+++ lib/src/widgets/tabs/query_groupings_tab/query_aggregates.dart
@@ -1,13 +1,78 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_gen/gen_l10n/query_wizard_localizations.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class QueryAggregates extends StatelessWidget {
+class QueryAggregates extends HookWidget {
   QueryAggregates({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final localizations = QueryWizardLocalizations.of(context);
-    return Text(localizations?.aggregates ?? 'Aggregates');
+    final showBottomSheetCallback = useState<VoidCallback?>(null);
+    final selectionListClosed = useState(true);
+    final mounted = useIsMounted();
+
+    void _showPersistentBottomSheet() {
+      showBottomSheetCallback.value = null;
+
+      Scaffold.of(context)
+          .showBottomSheet<void>(
+            (context) {
+              selectionListClosed.value = false;
+              return _SelectionList();
+            },
+            elevation: 25,
+          )
+          .closed
+          .whenComplete(() {
+            if (mounted()) {
+              selectionListClosed.value = true;
+              showBottomSheetCallback.value = _showPersistentBottomSheet;
+            }
+          });
+    }
+
+    if (selectionListClosed.value) {
+      showBottomSheetCallback.value = _showPersistentBottomSheet;
+    }
+
+    return Center(
+      child: ElevatedButton(
+        onPressed: showBottomSheetCallback.value,
+        child: Text('Select'),
+      ),
+    );
+  }
+}
+
+class _SelectionList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 500,
+      child: Column(
+        children: [
+          Container(
+            height: 70,
+            child: Center(
+              child: Text(
+                'Header',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          const Divider(thickness: 1),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 21,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(index.toString()),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/src/widgets/tabs/query_groupings_tab/query_groupings.dart
+++ lib/src/widgets/tabs/query_groupings_tab/query_groupings.dart
@@ -1,15 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-import 'package:flutter_gen/gen_l10n/query_wizard_localizations.dart';
-
-class QueryGroupings extends StatelessWidget {
+class QueryGroupings extends HookWidget {
   QueryGroupings({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final localizations = QueryWizardLocalizations.of(context);
-    return Text(
-      localizations?.groupings ?? 'Groupings',
+    final showBottomSheetCallback = useState<VoidCallback?>(null);
+    final selectionListClosed = useState(true);
+    final mounted = useIsMounted();
+
+    void _showPersistentBottomSheet() {
+      showBottomSheetCallback.value = null;
+
+      Scaffold.of(context)
+          .showBottomSheet<void>(
+            (context) {
+              selectionListClosed.value = false;
+              return _SelectionList();
+            },
+            elevation: 25,
+          )
+          .closed
+          .whenComplete(() {
+            if (mounted()) {
+              selectionListClosed.value = true;
+              showBottomSheetCallback.value = _showPersistentBottomSheet;
+            }
+          });
+    }
+
+    if (selectionListClosed.value) {
+      showBottomSheetCallback.value = _showPersistentBottomSheet;
+    }
+
+    return Center(
+      child: ElevatedButton(
+        onPressed: showBottomSheetCallback.value,
+        child: Text('Select'),
+      ),
+    );
+  }
+}
+
+class _SelectionList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 500,
+      child: Column(
+        children: [
+          Container(
+            height: 70,
+            child: Center(
+              child: Text(
+                'Header',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          const Divider(thickness: 1),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 21,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(index.toString()),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
