bc84fbce0dd2cf53fd9747aac17caa9be71cff86
Dmitrii Kiselev <dmitrii.kiselev.dev@gmail.com>
2021-05-22 14:53:29 +0300

    feat(lib): add `QueryUnionsAliasesTab` tab

---
+++ lib/src/widgets/tabs/query_unions_aliases_tab/query_aliases.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QueryAliases extends StatelessWidget {
+  const QueryAliases({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Aliases');
+  }
+}
